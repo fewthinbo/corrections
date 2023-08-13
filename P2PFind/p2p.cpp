fba96d9bb556f9810f4bef762070e1752a0d08eb
fewthinbo <zyfluxor@gmail.com>
2023-08-13 19:46:06 +0300

    Update p2p.cpp

--- P2PFind/p2p.cpp
+++ P2PFind/p2p.cpp
@@ -5,8 +5,8 @@ diff --git a/P2P.Find/p2p.cpp b/P2P.Find/p2p.cpp
  {
  	TCCIMap::const_iterator it;
  
--	if(false/* false == LC_IsBrazil()*/)
-+	if(false == LC_IsBrazil())
++	if(false/* false == LC_IsBrazil()*/)
+-	if(false == LC_IsBrazil())
  	{
  		it = m_map_pkCCI.find(c_pszName);
  	}
