09af5626427c29243baf80618732d6eb6b10ed0c
zyfluxor <141745715+fewthinbo@users.noreply.github.com>
2023-08-14 14:07:10 +0300

    Delete p2p.cpp

--- P2PFind/p2p.cpp
+++
@@ -1,9 +0,0 @@
-in CCI * P2P_MANAGER::Find(const char * c_pszName)
- {
- 	TCCIMap::const_iterator it;
- 
-+	if(false/* false == LC_IsBrazil()*/)
--	if(false == LC_IsBrazil())
- 	{
- 		it = m_map_pkCCI.find(c_pszName);
- 	}
